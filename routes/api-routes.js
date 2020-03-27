dbc2e7c38ef5ec551e6cfb904db101dc767e4ef6
drthisguy <page.c.tyler@gmail.com>
2020-03-27 12:28:30 -0400

    api routes completed. seeder loaded.

---
+++ routes/api-routes.js
@@ -0,0 +1,52 @@
+const Workout = require('../models/workout');
+
+
+module.exports = function(router) {
+
+// create new workout
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then( dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch( err => {
+            res.status(400).json(err);
+        });
+});
+
+// get last workout
+router.get("/api/workouts", (req, res) => {
+    Workout.findOne({})
+        .then(dbWorkout => {
+        res.json(dbWorkout);
+        })
+        .catch(err => {
+        res.status(400).json(err);
+        });
+});
+
+// get all workouts
+router.get("/api/workouts/range", (req, res) => {
+    Workout.findAll({})
+        .sort({ _id: -1 })
+        .then(dbWorkout => {
+        res.json(dbWorkout);
+        })
+        .catch(err => {
+        res.status(400).json(err);
+        });
+});
+
+// add exercise to workout. 
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        {_id: req.params.id}, 
+        {$push: {exercises: req.body}}
+        ).then(dbWorkout => {
+        res.json(dbWorkout);
+        })
+        .catch(err => {
+        res.status(400).json(err);
+        });
+});
+}
--- server.js
+++ server.js
@@ -18,7 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
     .catch(  err => {console.log(err)});
 
 // routes
-app.use(require("./routes/api-routes"));
+// require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
